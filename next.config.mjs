03b7e30952c319b4b3f1e8dad604c7234d6f1b46
Bikash Sarker <bikash.webdeveloper5@gmail.com>
2026-02-10 16:04:48 +0600

    the company detals page create annd problem slove

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,11 @@ const nextConfig = {
         hostname: "pub-b7fd9c30cdbf439183b75041f5f71b92.r2.dev",
         port: "",
       },
+      {
+        protocol: "https",
+        hostname: "water-analysis.s3.eu-north-1.amazonaws.com",
+        pathname: "/**",
+      },
     ],
   },
 };
--- src/app/super-admin/company-management/company-details/page.tsx
+++ src/app/super-admin/company-management/company-details/page.tsx
@@ -1,5 +1,4 @@
 import CompanyDetails from "@/components/companyManagement/details/CompanyDetails";
-import React from "react";
 
 const page = () => {
   return (
